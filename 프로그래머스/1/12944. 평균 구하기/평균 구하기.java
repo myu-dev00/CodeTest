f7d1a064d4437095f063ae6aa5aaaea873f7ba33
myu <myu.dev00@gmail.com>
2025-04-29 09:13:16 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer=0;
+        int cnt=0;
+        for(int a : arr){
+            answer+=a;
+            ++cnt;
+        }
+        return answer = answer /cnt;
+    }
+}
